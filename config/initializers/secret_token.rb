e8a4e62a5345a43c656ed18a78f5a999423a2071
itzamacko <anthony.marchante@me.com>
2012-07-05 14:43:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metamune::Application.config.secret_token = 'bae1368d4343969f1fc10f2b94b5cebcfb6d5818e0a4a39f32cd25d250e6dbb08a42857fee287326c4027c2ff066015879b8a8d17c7af3a00d0b6c4845f7595d'
